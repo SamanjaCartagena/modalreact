153e2ebf1045a55647031bce4a95b867cbb500d4
Samanja Cartagena <c.samanja09@Pabs.local>
2025-02-11 21:32:53 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import {useState} from 'react'
+import Modal from './components/Modal'
 function App() {
+  const [openModal, setOpenModal] = useState(false)
+  
   return (
     <div className="App">
       <h1>Hey, Click on the button to open the modal</h1>
-      <button className='openModalBtn'>Open</button>
+      <button className='openModalBtn' onClick={() =>setOpenModal(true)}>Open</button>
+      {openModal &&<Modal closeModal={setOpenModal}/>}
     </div>
   );
 }
