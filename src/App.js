68d6bfb750b5436f83cba293e511495c499a5b8d
Samanja Cartagena <c.samanja09@Pabs.local>
2025-02-11 20:38:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey, Click on the button to open the modal</h1>
+      <button className='openModalBtn'>Open</button>
+    </div>
+  );
+}
+
+export default App;
